1b0087d3068d32c55deb603c0a1efe1cbc39ac4f
Nahom Alemu <128564728+NahomAl@users.noreply.github.com>
2024-12-30 11:02:58 +0300

    updated content descriptions for venue selection, invitation card, entertainment, and photos/videos sections

--- index.php
+++ index.php
@@ -74,38 +74,27 @@
             <div class="box">
                 <i class="fas fa-map-marker-alt"></i>
                 <h3>venue selection</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, suscipit.</p>
+                <p>we 24/7 in the offices</p>
             </div>
 
             <div class="box">
                 <i class="fas fa-envelope"></i>
                 <h3>invitation card</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, suscipit.</p>
+                <p>we will help out with our emails</p>
             </div>
 
             <div class="box">
                 <i class="fas fa-music"></i>
                 <h3>entertainment</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, suscipit.</p>
-            </div>
-
-            <div class="box">
-                <i class="fas fa-utensils"></i>
-                <h3>food and drinks</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, suscipit.</p>
+                <p>We provide listening exam</p>
             </div>
 
             <div class="box">
                 <i class="fas fa-photo-video"></i>
                 <h3>photos and videos</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, suscipit.</p>
+                <p>We provide courses in YouTube</p>
             </div>
 
-            <div class="box">
-                <i class="fas fa-birthday-cake"></i>
-                <h3>custom food</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, suscipit.</p>
-            </div>
 
         </div>
 
